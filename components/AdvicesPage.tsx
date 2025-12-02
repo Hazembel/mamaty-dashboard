9c4b024e74cc89e8d4af02e66024efdd8a1c8220
Hazem Belhadj Salah <hazembelhadjsalah@gmail.com>
2025-12-02 19:53:48 +0100

    test

--- components/AdvicesPage.tsx
+++ components/AdvicesPage.tsx
@@ -38,7 +38,7 @@ const Toast: React.FC<{ message: string; type: 'success' | 'error'; onClose: () 
 };
 
 const AGE_TABS = [
-    { id: 'all', label: 'Reset', min: -1, max: -1 },
+    { id: 'all', label: 'Les restes', min: -1, max: -1 },
     { id: '6-9', label: '6 - 9 mois (180 - 270 j)', min: 180, max: 270 },
 ];
 
