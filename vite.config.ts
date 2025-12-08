2d1365c249963632ff7ff1c380628854f3714991
Hazem Belhadj Salah <hazembelhadjsalah@gmail.com>
2025-12-08 16:49:31 +0100

    add base: '/mamaty-dashboard/'

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
       host: "0.0.0.0",
     },
     plugins: [react()],
-    base: '/', // ⚠ relative paths for cPanel
+   base: '/mamaty-dashboard/',
     define: {
       "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
       "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
